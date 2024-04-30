d0a14582a12fa232493199bbff0f3a705ef9a56c
Nathaniel Praytor <168062889+ExceptionUnknown@users.noreply.github.com>
2024-04-30 07:59:33 -0500

    Update PromptCollection_SimpleEnough_NF.ps1
    changed references to ANSI Escape sequences from the '$wansi' Variable I used to the actual escape sequences, to reduce dependencies

--- PromptCollection_SimpleEnough_NF.ps1
+++ PromptCollection_SimpleEnough_NF.ps1
@@ -1,10 +1,10 @@
 function Prompt {
     $lo = Split-Path $PWD -NoQualifier
     if ($isAdmin) {
-        Write-Host "$($Wansi.Bold)$($Wansi.F75)[  $env:USERNAME / $($Wansi.F191) ADMIN$($Wansi.F75)]$($Wansi.R)" 
+        Write-Host "`e[1m`e[38;5;75m[  $env:USERNAME / `e[38;5;191m ADMIN`e[38;5;75m ]`e[0m" 
     } else {
-        Write-Host "$($Wansi.F75)[$env:USERNAME / $($Wansi.F251) USER$($Wansi.F75)]"
+        Write-Host "`[1m`e[38;5;75m[  $env:USERNAME / `[e38;5;251m  USER`e[38;5;75m ]`e[0m"
     }
-    Write-Host "[ ~$lo]" -nonewline -ForegroundColor yellow
-    return "$($Wansi.F117) _ $($Wansi.R)"
+    Write-Host "[  ~$lo ]" -nonewline -ForegroundColor yellow
+    return "`e[38;5;117m _ `e[0m"
     }
